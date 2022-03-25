eeb81def3a7d8c57edb4f019a0b758bb6f6ea609
Malu <maluvbmelo@gmail.com>
2022-03-25 09:47:09 -0300

    feat: add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,16 @@
+import { AccessToken } from "../models"
+import { AuthenticationError } from "../errors"
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
+
+
